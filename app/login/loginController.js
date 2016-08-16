54f91a2e3db87f533e7265f430ed39288da4fa24
Gustavo Pacheco <ryctabo@gmail.com>
2016-08-15 23:50:27 -0500

    Add loader for login

--- app/login/loginController.js
+++ app/login/loginController.js
@@ -5,16 +5,20 @@ moduleUdc.controller('loginController', ['$scope', '$state', 'apiService', funct
   
   $scope.form = { username: '', password: '' };
   $scope.unauthorizate = false;
+  $scope.loginProgress = false;
   
   $scope.login = function () {
+    $scope.loginProgress = true;
     $scope.unauthorizate = false;
     apiService.accounts.login($scope.form).then(
       function (result) {
         sessionStorage.authToken = result.data.token;
         $state.go('mainIndex');
+        $scope.loginProgress = false;
       },
       function (error) {
         console.error(error);
+        $scope.loginProgress = false;
         if (error.status === 401) {
           $scope.unauthorizate = true;
         }
