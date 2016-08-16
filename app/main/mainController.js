5956cac5d2ef0449cb6ed2c1e7f021881f5f364e
Gustavo Pacheco <ryctabo@gmail.com>
2016-08-15 23:44:31 -0500

    Add new sessionInfo

--- app/main/mainController.js
+++ app/main/mainController.js
@@ -1,7 +1,13 @@
 moduleUdc.controller('mainController', ['$scope', '$state', function ($scope, $state) {
-  if(!sessionStorage.authToken)
+  if (!sessionStorage.authToken)
     $state.go('login');
-  
+
+  $scope.sessionInfo = {};
+  if (sessionStorage.authToken) {
+    var encodedUserInfo = sessionStorage.authToken.split('.')[1];
+    $scope.sessionInfo = JSON.parse(atob(encodedUserInfo));
+  }
+
   $scope.logout = function () {
     delete sessionStorage.authToken;
     $state.go('login');
