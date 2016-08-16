af859b3a728efdcf7628b370216aa3c24d03af65
Gustavo Pacheco <ryctabo@gmail.com>
2016-08-16 02:50:36 -0500

    Now you can download the document

--- app/documents/documentsController.js
+++ app/documents/documentsController.js
@@ -1,3 +1,4 @@
 moduleUdc.controller('documentsController', ['$scope', 'apiService', function ($scope, apiService) {
   $scope.documents = apiService.documents.getAll();
+  $scope.token = sessionStorage.authToken;
 }]);
