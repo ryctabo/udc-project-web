70ce31a5c8987363dc0cf146c86f201e95150d58
Gustavo Pacheco <ryctabo@gmail.com>
2017-09-13 10:17:27 -0500

    Add search in the document component

--- app/documents/documentsController.js
+++ app/documents/documentsController.js
@@ -1,4 +1,14 @@
 moduleUdc.controller('documentsController', ['$scope', 'apiService', function ($scope, apiService) {
-  $scope.documents = apiService.documents.getAll();
+  let allDocuments = apiService.documents.getAll();
+  $scope.documents = allDocuments;
   $scope.token = sessionStorage.authToken;
+  $scope.searchValue = '';
+
+  $scope.search = function (value) {
+    value = value.toLowerCase();
+    $scope.documents = allDocuments;
+    $scope.documents = $scope.documents.filter(function (d) {
+      return d.name.toLowerCase().indexOf(value) !== -1;
+    });
+  };
 }]);
